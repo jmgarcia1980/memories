8bd943214cadcbe2fae7dd2d4bf1c01b43d8274a
jmgarcia1980 <mail.jgarcia@gmail.com>
2020-03-11 15:15:26 +0000

    20200311 Nuevos cambios

--- memories/src/components/memcontainer/MemPicture.js
+++ memories/src/components/memcontainer/MemPicture.js
@@ -3,19 +3,24 @@ import './styles.css';
 
 
 const MemPicture = () => (
-    <div>
-        <ul>
+    <div className="uk-position-relative uk-visible-toggle uk-light" uk-slideshow="ratio: 7:2; min-height: 300; max-height: 350; animation: push">
+        <ul className="uk-slideshow-items">
             <li>
-                Imagen1A
+                <img ClassName="imgMemPicture" src="static/images/01.jpg" width="300px" height="300px" alt=""/>
             </li>
             <li>
-                Imagen2
+                <img ClassName="imgMemPicture" src="static/images/02.jpg" alt="" width="300px" height="300px"/>
             </li>
             <li>
-                Imagen3
+                <img ClassName="imgMemPicture" src="static/images/03.jpg" alt="" width="300px" height="300px"/>
             </li>
-        </ul>
+            <li>
+                <img ClassName="imgMemPicture" src="static/images/01A.jpg" alt="" uk-cover="true"/>
+            </li>
 
+        </ul>
+        <a className="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous="true" uk-slideshow-item="previous"></a>
+        <a className="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next="true" uk-slideshow-item="next"></a>
     </div>
 
 );
